fbf7b8d37394fc7d37c4d7c64b16e413a8c3268c
inicolai01 <irina.nic.yu@gmail.com>
2023-03-26 12:08:46 +0200

    Create CounterButtonApp

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,11 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const CounterButtonApp = createApp({
+  data() {
+    return {
+      count: 0,
+    }
+  },
+});
+
+CounterButtonApp.mount('#app');
