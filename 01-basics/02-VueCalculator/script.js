b242983a2996eb876c4bbf7bcfa71689604cd929
inicolai01 <irina.nic.yu@gmail.com>
2023-03-26 13:33:53 +0200

    Create VueCalculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,42 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const VueCalculator = createApp({
+  data() {
+    return {
+      firstNumber: 0,
+      secondNumber: 0,
+      operator: 'sum',
+    }
+  },
+
+  computed: {
+    result() {
+      return this.calculateResult(this.operator, this.firstNumber, this.secondNumber)
+    }
+  },
+
+  methods: {
+    calculateResult(operation, a, b) {
+      switch (operation) {
+        case 'sum':
+          return a + b;
+
+        case 'subtract':
+          return a - b;
+
+        case 'multiply':
+          return a * b;
+
+        case 'divide': {
+          if (b === 0) {
+            alert('You cannot divide on zero');
+          } else {
+            return a / b;
+          }
+        }
+      }
+    }
+  }
+});
+
+VueCalculator.mount('#app');
